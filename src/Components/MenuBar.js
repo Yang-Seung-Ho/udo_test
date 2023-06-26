fcabafbbab7998a4b85760260fe42db82f360553
Yang-Seung-Ho <ju5979@jejunu.ac.kr>
2023-06-26 14:37:32 +0900

    dd

--- src/Components/MenuBar.js
+++ src/Components/MenuBar.js
@@ -1,7 +1,7 @@
 import styled, { css } from "styled-components";
 import { Link, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
-import Logo from "../Images/Logo.png";
+import Logo from "../Images/logo.png";
 const MenuBarBox = styled.div`
   position: fixed;
   font-family: "NanumSquareNeoLight";
